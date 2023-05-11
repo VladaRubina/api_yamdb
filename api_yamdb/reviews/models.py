ab445ee117a564a5e33377a61ca187a382124101
Алексей Россомахин <alexeirossomahin@yandex.ru>
2023-05-11 16:33:11 +0300

    1

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+
+class Title(models.Model):
+    pass
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+    text = models.TextField()
+
+    class Meta:
+        pass
