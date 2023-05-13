428bc29197de1c031e22e7c2b1ae68f532de356c
Vlada Rubina <vlada.v.rubina@gmail.com>
2023-05-14 00:12:51 +0300

    Categories, genres, titles

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'genres', GenreViewSet)
+router.register(r'titles', TitleViewSet)
